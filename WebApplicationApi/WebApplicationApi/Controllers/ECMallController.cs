ac674a55e3387b873b750763cc00b177e2f13fad
eminthwu <andrew.liu@advantech.com.tw>
2018-02-26 12:00:14 +0800

    finish 1, 2, and login function of 4

---
+++ WebApplicationApi/WebApplicationApi/Controllers/ECMallController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplicationApi.Models;
+
+namespace WebApplicationApi.Controllers
+{
+    [RoutePrefix("EC")]
+    public class ECMallController : ApiController
+    {
+        [Route("Login/{account}")]
+        [HttpPost]
+        public bool Login(string account, [FromBody]string password)
+        {
+            Entity.ECMallEntities1 entity = new Entity.ECMallEntities1();
+            var result = entity.UserInfo.Where(u => u.Account == account && u.Password == password).FirstOrDefault();
+            return result != null;
+        }
+
+        [Route("AddProduct")]
+        [HttpPost]
+        public int InsertProduct(Product product)
+        {
+            if (product == null)
+                return 0;
+
+            WebApplicationApi.Entity.Product insertP = new Entity.Product()
+            {
+                ProductCategoryId = product.ProductCategoryId,
+                ID = System.Guid.NewGuid(),
+                Name = product.Name,
+                Price = product.Price,
+                Amount = product.Amount
+            };
+
+            Entity.ECMallEntities1 entity = new Entity.ECMallEntities1();
+            entity.Product.Add(insertP);
+            var result = entity.SaveChanges();
+
+            return result;
+        }
+
+        [Route("DeleteProduct")]
+        [HttpDelete]
+        public int RemoveProduct(Guid productId)
+        {
+            if (productId == null)
+                return 0;
+
+            Entity.ECMallEntities1 entity = new Entity.ECMallEntities1();
+            entity.Product.Remove(new Entity.Product() { ID = productId });
+            var result = entity.SaveChanges();
+
+            return result;
+        }
+
+        [Route("EditProduct")]
+        [HttpPatch]
+        public int AlterProduct(Product product)
+        {
+            if (product == null || product.ID == null)
+                return 0;
+
+            Entity.ECMallEntities1 entity = new Entity.ECMallEntities1();
+            var alterProduct = entity.Product.Where(p => p.ID == product.ID).FirstOrDefault();
+
+            if (alterProduct == null)
+                return 0;
+
+            alterProduct.Name = product.Name;
+            alterProduct.Price = product.Price;
+            alterProduct.Amount = product.Amount;
+
+            var result = entity.SaveChanges();
+
+            return result;
+        }
+
+        [Route("QueryProduct")]
+        [HttpGet]
+        public Product QueryProduct(Guid productId)
+        {
+            if (productId == null)
+                return null;
+
+            Entity.ECMallEntities1 entity = new Entity.ECMallEntities1();
+            var product = entity.Product.Where(p => p.ID == productId).FirstOrDefault();
+
+            return product == null ? null : new Product()
+            {
+                ProductCategoryId = product.ProductCategoryId,
+                ID = System.Guid.NewGuid(),
+                Name = product.Name,
+                Price = product.Price,
+                Amount = product.Amount
+            };
+        }
+    }
+}
---
+++ WebApplicationApi/WebApplicationApi/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationApi.Models
+{
+    public class Product
+    {
+        public System.Guid ID { get; set; }
+
+        public int ProductCategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public int? Price { get; set; }
+
+        public int? Amount { get; set; }
+    }
+}
